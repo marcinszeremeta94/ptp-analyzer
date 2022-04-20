1e8de5080cb990b4d9c6604eae787af3ad7f8997
Marcin Szeremeta <marcin.szeremeta.94@gmail.com>
2022-04-20 03:45:02 +0200

    added part of a test

--- mptp/PtpCheckers/PtpCheckers_tests/PtpSequenceId_test.py
+++ mptp/PtpCheckers/PtpCheckers_tests/PtpSequenceId_test.py
@@ -1,3 +1,5 @@
+from os import stat
+from mptp.PtpPacket.PTPv2 import PTPv2, PTP_MSG_TYPE
 from tests.testutils.DummyLogger import DummyLogger
 from mptp.PtpCheckers.PtpSequenceId import PtpSequenceId
 import unittest
@@ -10,12 +12,28 @@ class PtpSequenceId_test(unittest.TestCase):
     def setUp(self):
         self.sut = PtpSequenceId(self.dummy_logger, self.dummy_time_offset)
     
-    def test1(self):
-        print(self.sut)
-        self.assertEqual(4, 4)
+    def test_is_cached_time_offset_correct(self):
         self.assertEqual(self.dummy_time_offset, self.sut.time_offset)
+    
+    def test_check_sync_followup_sequence(self):
+        self.assertEqual(self.dummy_time_offset, self.sut.time_offset)
+        sync, fup = PtpSequenceId_test.create_ptp_sync_fup_test_data()
+        self.sut.check_sync_followup_sequence(sync, fup)    
+    
+    @staticmethod  
+    def create_ptp_sync_fup_test_data(n: int = 10):
+        sync: list[PTPv2] = []
+        fup: list[PTPv2] = []
+        for i in range(0, n):
+            sync.append(PTPv2())
+            sync[-1].messageType = PTP_MSG_TYPE.SYNC_MSG.value
+            sync[-1].sequenceId = i
+            fup.append(PTPv2())
+            fup[-1].messageType = PTP_MSG_TYPE.FOLLOW_UP_MSG.value
+            fup[-1].sequenceId = i  
+        return (sync, fup)
+        
     
 if __name__ == '__main__':
     unittest.main()
 
-# python -m mptp.PtpCheckers.PtpCheckers_tests.PtpSequenceId_test
